d635cf980660e932294d7dd5f9dc7ed6b3cd1410
kgtrue <gammel4@gmail.com>
2023-02-25 22:58:23 +0100

    Some more stuff

--- src/Dictionary-Bench/Benchmarks.cs
+++ src/Dictionary-Bench/Benchmarks.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Runtime.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,18 +8,18 @@ using System.Threading.Tasks;
 
 namespace Dictionary_Bench
 {
-    
+
     [MemoryDiagnoser]
     [RankColumn]
     public class Benchmarks
     {
         public List<IntEntity> Ints { get; set; }
-            
+
 
         [Benchmark]
         public int WithIntEntityToDictionary()
-        {           
-          var dic=  Ints.ToDictionary(x => x.Value);
+        {
+            var dic = Ints.ToDictionary(x => x.Value);
             return dic.Count;
         }
 
@@ -42,12 +43,37 @@ namespace Dictionary_Bench
         public int WithIntEntityToDictionaryGrowingForEach()
         {
             var dic = new Dictionary<int, IntEntity>();
-            foreach(var ie in Ints)
+            foreach (var ie in Ints)
             {
                 dic.Add(ie.Value, ie);
             }
             return dic.Count;
-        }       
+        }
+
+
+        [Benchmark]
+        public int WithIntEntityToDictionaryGrowingForLoop()
+        {
+            var dic = new Dictionary<int, IntEntity>();
+            for (int i = -10000; i <= 10000; i++)
+            {
+                var item = new IntEntity(i);
+                dic.Add(item.Value, item);
+            }
+            return dic.Count;
+        }
+
+        [Benchmark]
+        public int WithIntEntityToDictionaryFixedCapacityForLoop()
+        {
+            var dic = new Dictionary<int, IntEntity>(200001);
+            for (int i = -10000; i <= 10000; i++)
+            {
+                var item = new IntEntity(i);
+                dic.Add(item.Value, item);
+            }
+            return dic.Count;
+        }
 
         private List<IntEntity> GetInts()
         {
